34fa805cda49ca3c7a27cf56b4d070aec648d6b0
Kai Davenport <kaiyadavenport@gmail.com>
2014-05-14 19:47:13 +0000

    vikingfile tests

--- test/vikingfile.js
+++ test/vikingfile.js
@@ -10,8 +10,33 @@ tape('vikingfile', function(t){
   file.load(function(err, data){
   	t.ok(!err, 'no error returned')
 
-  	console.log('-------------------------------------------');
-  	console.dir(data)
+  	t.equal(data.config.name, 'ragnar')
+  	t.equal(data.config.comment, 'Ragnar Hamster Lothbrok')
+
+  	t.deepEqual(data.buildOrder, ['src', 'inherit'])
+  	t.deepEqual(data.bootOrder, ['db', 'mongo', 'redis', 'logic', 'website', 'help'])
+
+  	t.deepEqual(data.images.src, {
+  		add:[{
+  			source:'.',
+  			dest:'/srv/app'
+  		}],
+  		expose:[],
+  		volume:[],
+  		run:[
+  			'cd /srv/app && npm install'
+  		],
+  		from:'quarry/monnode'
+  	})
+
+    t.deepEqual(data.containers.db, {
+      image:'viking:ragnar/src',
+      expose:[80],
+      volume:['/data/db'],
+      run:'mon node index.js --volume=/data/db',
+      cwd:'/srv/app/db'
+    })
+
   	t.end()
   })
 
