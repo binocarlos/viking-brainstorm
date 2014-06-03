72e65dd44c227bf2f1e192bb818e8eb75309aa2d
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-03 13:03:41 +0000

    change registry config to plain sync object

---
+++ test/container.js
@@ -0,0 +1,24 @@
+var spawn = require('child_process').spawn
+var exec = require('child_process').exec
+var etcdjs = require('etcdjs')
+var flatten = require('etcd-flatten')
+var tape     = require('tape')
+var config = require('../lib/config')()
+var Container = require('../lib/tools/container')
+var concat = require('concat-stream')
+var Registry = require('../lib/services/registry')
+var endpoints = require('../lib/tools/endpoints')
+var Job = require('../lib/tools/job')
+var tools = require('./lib/tools')
+var state = {}
+
+
+tape('get the docker arguments', function(t){
+
+	var container = Container({
+		
+	}, config)
+	
+  
+
+})
