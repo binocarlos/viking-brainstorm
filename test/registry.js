ac0ca5589a68b76aa5fd5ca7413f54880d04a234
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-03 10:42:05 +0000

    registry test

--- test/registry.js
+++ test/registry.js
@@ -5,6 +5,7 @@ var flatten = require('etcd-flatten')
 var tape     = require('tape')
 var config = require('../lib/config')()
 var concat = require('concat-stream')
+var Registry = require('../lib/services/registry')
 var tools = require('./lib/tools')
 var state = {}
 
@@ -22,7 +23,37 @@ etcdserver.check(tape)
 
 tape('registry config', function(t){
 
-	t.end()
+	
+
+	Registry(config, etcd, function(err, settings){
+		if(err){
+			t.fail(err, 'get registry settings')
+			t.end()
+			return
+		}
+
+		t.equal(settings.stack, 'core', 'stack=core')
+		t.equal(settings.name, 'registry', 'name=registry')
+		t.equal(settings.image, 'registry', 'image=registry')
+
+		var systemFilter = settings.filter.filter(function(f){
+			return f.tag=='system'
+		})[0]
+
+		t.ok(systemFilter, 'there is a system filter on the registry')
+
+		t.equal(settings.env.DOCKER_REGISTRY_CONFIG, '/registryconfig/config.yml', 'the config path is set')
+		t.equal(settings.env.SETTINGS_FLAVOR, 'development', 'development mode')
+
+		t.equal(settings.ports.length, 1, '1 port')
+		t.equal(settings.ports[0], '5000:5000', 'expose 5000')
+
+		t.equal(settings.volumes.length, 2, '2 volumes')
+
+		t.equal(settings.volumes[0], '/srv/projects/viking/files/registry:/registryconfig', 'conf volume')
+		t.equal(settings.volumes[1], '/var/lib/viking/volumes/core/data/registry:/data/registry', 'data volume')
+		t.end()
+	})
 	
   
 
