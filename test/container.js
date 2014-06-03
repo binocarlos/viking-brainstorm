acfd33fd01ca648a3569168f1c511a27a54058fa
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-03 14:13:59 +0000

    container config tests

--- test/container.js
+++ test/container.js
@@ -11,13 +11,14 @@ var endpoints = require('../lib/tools/endpoints')
 var Job = require('../lib/tools/job')
 var Volume = require('../lib/tools/volume')
 var DockerRun = require('../lib/tools/dockerrun')
+
 var tools = require('./lib/tools')
 var state = {}
 
 function getJob(){
 	var vol = Volume(config, 'test', '/test1/store')
 
-	var conf =  {
+	var job =  {
 	  stack:'test',
 	  name:'test1',
 	  image:'quarry/monnode',
@@ -32,26 +33,36 @@ function getJob(){
 	  cwd:'/'
 	}
 
-	var job = getContainerDesc()
 	var jobObject = Job(job)
 	jobObject.ensureValues()
 
-	return job._data
+	return jobObject.data()
 }
 
 tape('dockerrun arguments', function(t){
-	var args = DockerRun(getContainerDesc())
+	var args = DockerRun(getJob())
 
-	console.log('-------------------------------------------');
-	console.dir(args)
+	t.equal(args.length, 12, 'there are 12 args')
+
+	t.deepEqual(args, [ '-t',
+	  '--name',
+	  'test/default/test1',
+	  '-v',
+	  '/var/lib/viking/volumes/test/test1/store:/test1/store',
+	  '-e',
+	  'TEST=10',
+	  'quarry/monnode',
+	  'echo',
+	  '$TEST',
+	  '>',
+	  '/test1/store/env' ], 'the args are correct')
+
 	t.end()
 })
 
-tape('get the docker arguments', function(t){
+tape('get the docker arguments from the container', function(t){
 
-	
-
-	var container = Container(job)
+	var container = Container(getJob())
 	
 	var options = container.options()
 
@@ -59,8 +70,6 @@ tape('get the docker arguments', function(t){
 	t.equal(options.name, 'test1', 'name')
 	t.equal(options.image, 'quarry/monnode', 'image')
 	t.equal(options.volumes[0], '/var/lib/viking/volumes/test/test1/store:/test1/store', 'volume')
-
-
 
 	t.end()
 
