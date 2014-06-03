1bd1cc0cb41cdc7599b642bbbaef4557c5e8ab5b
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-03 14:22:59 +0000

    allow entrypoint workdir options

--- test/container.js
+++ test/container.js
@@ -15,6 +15,35 @@ var DockerRun = require('../lib/tools/dockerrun')
 var tools = require('./lib/tools')
 var state = {}
 
+
+function getArgsTest(){
+	var vol = Volume(config, 'test', '/test1/store')
+
+	var job =  {
+	  stack:'test',
+	  name:'test1',
+	  image:'quarry/monnode',
+	  env:{
+	    TEST:10
+	  },
+	  volumes:[
+	    vol
+	  ],
+	  dockerargs:[
+	  	'-m',
+	  	'100m'
+	  ],
+	  entrypoint:'/bin/bash',
+	  command:'echo $TEST > /test1/store/env',
+	  cwd:'/home'
+	}
+
+	var jobObject = Job(job)
+	jobObject.ensureValues()
+
+	return jobObject.data()
+}
+
 function getJob(){
 	var vol = Volume(config, 'test', '/test1/store')
 
@@ -69,10 +98,7 @@ function getServer(){
 tape('dockerrun arguments', function(t){
 	var args = DockerRun(getJob())
 
-	console.log('-------------------------------------------');
-	console.log('-------------------------------------------');
-	console.dir(args)
-	t.equal(args.length, 12, 'there are 12 args')
+	t.equal(args.length, 16, 'there are 16 args')
 
 	t.deepEqual(args, [ '-t',
 	  '--name',
@@ -81,6 +107,10 @@ tape('dockerrun arguments', function(t){
 	  '/var/lib/viking/volumes/test/test1/store:/test1/store',
 	  '-e',
 	  'TEST=10',
+	  '--entrypoint',
+	  '/bin/bash',
+	  '--workdir',
+	  '/',
 	  'quarry/monnode',
 	  'echo',
 	  '$TEST',
