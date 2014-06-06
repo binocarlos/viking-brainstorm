ee36bf3164b32650dc7982779476f0fc9f7c9773
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-06 16:49:50 +0000

    /run checking

--- test/deployment.js
+++ test/deployment.js
@@ -5,6 +5,7 @@ var etcdjs = require('etcdjs')
 var stubs = require('./lib/stubs')
 var flatten = require('etcd-flatten')
 var etcdserver = tools.etcd()
+var exec = require('child_process').exec
 
 var etcd = etcdjs('127.0.0.1:4001')
 
@@ -21,11 +22,9 @@ stubwriter.singlenetwork(etcd, tape)
 
 core.deploy(tape)
 
+var pid = null
 tape('check the proc deployment', function(t){
 
-	console.log('-------------------------------------------');
-	console.log('-------------------------------------------');
-	console.dir('check /proc')
 	etcd.get('/proc', {
 		recursive:true
 	}, function(err, result){
@@ -54,13 +53,42 @@ tape('check the proc deployment', function(t){
 		t.equal(job.id, 'core-default-registry-' + job.pid, 'id')
 		t.equal(key, '/proc/core/default/registry/' + job.pid, 'key')
 
+		pid = job.pid
 
-		
-
-		console.log('-------------------------------------------');
-		console.log('-------------------------------------------');
-		console.dir(result)
 		t.end()
+	})
+})
+
+tape('run the dispatch', function(t){
+
+	exec('viking dispatch', function(err, stdout){
+		if(err){
+			t.fail(err, 'dispatch')
+			t.end()
+			return
+		}
+		console.log(stdout.toString())
+		t.pass('dispatch')
+		t.end()
+	})
+
+})
+
+tape('check the dispatch /run', function(t){
+
+	etcd.get('/run', {
+		recursive:true
+	}, function(err, result){
+		if(err){
+			t.fail(err, 'load proc data')
+			t.end()
+			return
+		}
+		result = flatten(result.node)
+		
+		t.equal(result['/run/core/default/registry/' + pid], 'viking-0', 'the /run is set to the hostname'
+		t.end()
+
 	})
 })
 
