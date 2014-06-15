0b9ef8439165467302fa200ad635997dd71b5c03
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-15 17:03:25 +0000

    deploy test with default tag

--- test/stack.js
+++ test/stack.js
@@ -1,6 +1,13 @@
 var Stack = require('../lib/stack')
 var config = require('../lib/config')()
 var tape     = require('tape')
+var tools = require('./lib/tools')
+var etcdjs = require('etcdjs')
+var flatten = require('etcd-flatten')
+var etcdserver = tools.etcd()
+var exec = require('child_process').exec
+
+var etcd = etcdjs('127.0.0.1:4001')
 
 
 function getStack(){
@@ -12,6 +19,13 @@ function getStack(){
 }
 
 
+
+etcdserver.stop(tape, true)
+etcdserver.reset(tape)
+etcdserver.start(tape)
+tools.pause(tape, 2)
+etcdserver.check(tape)
+
 tape('boot order', function(t){
   var stack = getStack()
   stack.load(function(){
@@ -22,3 +36,47 @@ tape('boot order', function(t){
   })
   
 })
+
+
+tape('deploy with default tag', function(t){
+
+  var dir = __dirname + '/example'
+
+  exec('viking deploy --app ' + dir, function(err, stdout, stderr){
+    if(err){
+      t.fail(err, 'deploy default tag')
+      return
+    }
+
+    if(stderr){
+      t.fail(stderr.toString(), 'deploy default tag')
+      return
+    }
+
+    console.log(stdout.toString())
+
+    etcd.get('/stack', {
+      recursive:true
+    }, function(err, result){
+
+      if(err || !result){
+        t.fail(err, 'load /stack')
+        return
+      }
+
+      result = flatten(result.node)
+
+      t.ok(result['/stack/ragnar/tags/default'], 'the stack has written itself')
+
+      var obj = JSON.parse(result['/stack/ragnar/tags/default'])
+
+      t.equal(obj.config.name, 'ragnar', 'the stack has been written in JSON')
+      
+      t.end()
+    })
+  })
+
+})
+
+
+etcdserver.stop(tape)
